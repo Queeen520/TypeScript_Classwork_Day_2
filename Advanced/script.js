091417637a754e0cef974231bf18ad2c244ffe1e
Isabella W <isabellawindhaber@gmail.com>
2022-10-06 12:15:39 +0200

    --

--- Advanced/script.js
+++ Advanced/script.js
@@ -1,1 +1,32 @@
 "use strict";
+class Vehicles {
+    constructor(specification, name, horsepower, yearOfProduction, price) {
+        this.specification = specification;
+        this.name = name;
+        this.horsepower = horsepower;
+        this.yearOfProduction = yearOfProduction;
+        this.price = price;
+    }
+}
+let input = new Vehicles("Car", "SUV", 154, 1998, 55000);
+console.table(input);
+class Motorbikes extends Vehicles {
+    constructor(specification, name, horsepower, yearOfProduction, price, kilometersLeft, numberOfSeats, fuelType) {
+        super(specification, name, horsepower, yearOfProduction, price);
+        this.kilometersLeft = kilometersLeft;
+        this.numberOfSeats = numberOfSeats;
+        this.fuelType = fuelType;
+    }
+}
+let input1 = new Motorbikes("Motorbike", "Ducati", 214, 1988, 155000, 15000, 1, "Petrol");
+console.table(input1);
+class Trucks extends Vehicles {
+    constructor(specification, name, horsepower, yearOfProduction, price, maxLoad, numberOfSeats, fuelType) {
+        super(specification, name, horsepower, yearOfProduction, price);
+        this.maxLoad = maxLoad;
+        this.numberOfSeats = numberOfSeats;
+        this.fuelType = fuelType;
+    }
+}
+let input2 = new Trucks("Transporting Truck", "Iveco S-Way", 154, 2003, 65000, 20000, 2, "Diesel");
+console.table(input2);
--- Advanced/script.ts
+++ Advanced/script.ts
@@ -2,6 +2,52 @@ interface Vehicles {
     specification: string,
     name: string,
     horsepower: number,
-    publishingYear: number,
-    price: number,
+    yearOfProduction: number,
+    price: number
 }
+
+class Vehicles {
+    constructor(specification: string, name: string, horsepower: number, yearOfProduction: number, price: number,) {
+        this.specification = specification;
+        this.name = name;
+        this.horsepower = horsepower;
+        this.yearOfProduction = yearOfProduction;
+        this.price = price;
+    }
+}
+
+let input = new Vehicles("Car", "SUV", 154, 1998, 55000);
+console.table(input);
+
+
+class Motorbikes extends Vehicles {
+    kilometersLeft: number;
+    numberOfSeats: number;
+    fuelType: string;
+    
+    constructor(specification: string, name: string, horsepower: number, yearOfProduction: number, price: number, kilometersLeft: number, numberOfSeats: number, fuelType: string) {
+    super(specification, name, horsepower, yearOfProduction, price)
+    this.kilometersLeft = kilometersLeft;
+    this.numberOfSeats = numberOfSeats;
+    this.fuelType = fuelType;
+    }
+}
+
+let input1 = new Motorbikes("Motorbike", "Ducati", 214, 1988, 155000, 15000, 1, "Petrol");
+console.table(input1);
+
+class Trucks extends Vehicles {
+    maxLoad: number;
+    numberOfSeats: number;
+    fuelType: string;
+    
+    constructor(specification: string, name: string, horsepower: number, yearOfProduction: number, price: number, maxLoad: number, numberOfSeats: number, fuelType: string) {
+    super(specification, name, horsepower, yearOfProduction, price)
+    this.maxLoad = maxLoad;
+    this.numberOfSeats = numberOfSeats;
+    this.fuelType = fuelType;
+    }
+}
+
+let input2 = new Trucks("Transporting Truck", "Iveco S-Way", 154, 2003, 65000, 20000, 2, "Diesel");
+console.table(input2);
