7e98e0e67fd344532664f79a52c45824f5ba7cc3
Isabella W <isabellawindhaber@gmail.com>
2022-10-06 15:27:21 +0200

    --

--- Advanced/script.js
+++ Advanced/script.js
@@ -30,3 +30,16 @@ class Trucks extends Vehicle {
 }
 let input2 = new Trucks("Iveco S-Way", 154, 2008, 65000, 5000, 2, "Diesel");
 console.table(input2);
+
+// checkbox is checked or not
+// checked property of the checkbox element if checked = true else false
+
+const RadioCheckBoxYes = document.querySelector('#yes');
+console.log(checkBoxYes.checked); // returns true if checked else returns false
+
+const RadioCheckBoxNo = document.querySelector('#no');
+console.log(checkBoxNo.checked); // returns true if checked else returns false
+
+const checkBoxMore = document.querySelector('#more');
+console.log(checkBoxMore.checked); // returns true if checked else returns false7
+
