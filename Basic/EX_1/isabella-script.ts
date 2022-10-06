84be82f571f4634b627989fe4c85e94f4ab1f7b4
Isabella W <isabellawindhaber@gmail.com>
2022-10-06 09:40:22 +0200

    --

---
+++ Basic/EX_1/isabella-script.ts
@@ -0,0 +1,3 @@
+/* Create a class constructor named Person that should hold information about 
+name, age,  jobTitle and have a function that will return a string 
+“Hello there, My name is (name) and I am (age) years old, and I am a (jobTitle)” */ 
